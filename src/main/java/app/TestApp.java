a6b646d239ed6fc75e401b4948179a58ff032fe8
Joey <osaka9595@gmail.com>
2023-08-02 17:04:51 +0800

    remove  username/ password/nickname

---
+++ src/main/java/app/TestApp.java
@@ -0,0 +1,56 @@
+package app;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import core.util.HibernateUtil;
+import web.member.pojo.Member;
+
+public class TestApp {
+	public static void main(String[] args) {
+		TestApp app = new TestApp();
+//		Member member = new Member();
+//		member.setUsername("使用者名稱");
+//		member.setPassword("密碼");
+//		member.setNickname("暱稱");
+//		app.insert(member);
+//		System.out.println(member.getId());
+		
+		System.out.println(app.deleteById(4));
+	}
+
+	public Integer insert(Member member) {
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		try {
+			Transaction transaction = session.beginTransaction();
+			session.persist(member);
+			transaction.commit();
+			return member.getId();
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+			return null;
+		}
+
+	}
+
+	
+	public int deleteById(Integer id) {
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		try {
+			Transaction transaction = session.beginTransaction();
+			Member member = session.get(Member.class, id);
+			session.remove(member);
+			transaction.commit();
+			return 1;
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+			return -1;
+		}
+
+	}
+}
--- src/main/java/web/member/pojo/Member.java
+++ src/main/java/web/member/pojo/Member.java
@@ -12,21 +12,25 @@ import core.pojo.Core;
 
 @Entity
 public class Member extends Core {
+	private static final long serialVersionUID = 1062017833925367218L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private static final long serialVersionUID = 1062017833925367218L;
+	
 	private Integer id;
 	private String username;
 	private String password;
 	private String nickname;
+	@Column(insertable = false)
 	private Boolean pass;
-	@Column (name = "ROLE_ID")
+	@Column (name = "ROLE_ID",insertable = false)
 	private Integer roleId;
+	@Column (insertable = false)
 	private String creator;
-	@Column(name = "CREATED_DATE")
+	@Column(name = "CREATED_DATE", insertable = false)
 	private Timestamp createdDate;
+	@Column (insertable = false)
 	private String updater;
-	@Column(name="LAST_UPDATED_DATE")
+	@Column(name="LAST_UPDATED_DATE", insertable = false)
 	private Timestamp lastUpdatedDate;
 
 	public Member() {
