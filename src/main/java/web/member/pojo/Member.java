d761a679ddedfd0a53d9b247f65667f7b67f2e32
Joey <osaka9595@gmail.com>
2023-08-02 11:39:52 +0800

    member - add mapping setting and register entity

--- src/main/java/web/member/pojo/Member.java
+++ src/main/java/web/member/pojo/Member.java
@@ -2,19 +2,31 @@ package web.member.pojo;
 
 import java.sql.Timestamp;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import core.pojo.Core;
 
+@Entity
 public class Member extends Core {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private static final long serialVersionUID = 1062017833925367218L;
 	private Integer id;
 	private String username;
 	private String password;
 	private String nickname;
 	private Boolean pass;
+	@Column (name = "ROLE_ID")
 	private Integer roleId;
 	private String creator;
+	@Column(name = "CREATED_DATE")
 	private Timestamp createdDate;
 	private String updater;
+	@Column(name="LAST_UPDATED_DATE")
 	private Timestamp lastUpdatedDate;
 
 	public Member() {
